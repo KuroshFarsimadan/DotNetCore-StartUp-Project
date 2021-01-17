0123acab04f06dcfd299ad8f1df65f9f272f2d3f
Kurosh Farsimadan <kurosh_farsimadan@yahoo.com>
2021-01-17 17:58:09 -0500

    Add files via upload

---
+++ Services/EmailService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Skeleton.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly ILogger<EmailService> _logger;
+
+        public EmailService(ILogger<EmailService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void SendEmail(string to, string cc, string subject, string body)
+        {
+            _logger.LogInformation(to + " " + cc + " " + subject + " " + body);
+        }
+    }
+}
---
+++ Services/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Skeleton.Services
+{
+    public interface IEmailService
+    {
+        void SendEmail(string to, string cc, string subject, string body);
+    }
+}
